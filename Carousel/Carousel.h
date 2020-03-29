3cbb7f6619252adec88b7fb6989037dcf68f349f
Serhiy Medvedyev <serhiy.medvedyev@nordstrom.com>
2020-03-29 20:45:34 +0300

    Initial project setup

---
+++ Carousel/Carousel.h
@@ -0,0 +1,19 @@
+//
+//  Carousel.h
+//  Carousel
+//
+//  Created by Serhiy Medvedyev on 29.03.2020.
+//  Copyright © 2020 Serhiy Medvedyev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Carousel.
+FOUNDATION_EXPORT double CarouselVersionNumber;
+
+//! Project version string for Carousel.
+FOUNDATION_EXPORT const unsigned char CarouselVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Carousel/PublicHeader.h>
+
+
---
+++ CarouselExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarouselExamples
+//
+//  Created by Serhiy Medvedyev on 29.03.2020.
+//  Copyright © 2020 Serhiy Medvedyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
